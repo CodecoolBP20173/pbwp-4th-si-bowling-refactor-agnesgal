b38d8344f4dcd369af69ed3d3dd5dc16c81d0aa0
Agnes Gal <galagnes277@gmail.com>
2017-11-26 20:24:22 +0100

    My work

--- bowling.py
+++ bowling.py
@@ -7,8 +7,6 @@ def score(game):
             result += 10 - last
         else:
             result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
         if frame < 10  and get_value(game[i]) == 10:
             if game[i] == '/':
                 result += get_value(game[i+1])
@@ -19,27 +17,27 @@ def score(game):
                 else:
                     result += get_value(game[i+2])
         last = get_value(game[i])
-        if not in_first_half:
-            frame += 1
-        if in_first_half == True:
+        if in_first_half:
             in_first_half = False
         else:
             in_first_half = True
+            frame += 1
         if game[i] == 'X' or game[i] == 'x':
             in_first_half = True
             frame += 1
     return result
 
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
+    if char_analyze(char):
         return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
+    elif char == 'X' or char == 'x' or char == '/':
         return 10
     elif char == '-':
         return 0
     else:
         raise ValueError()
+
+def char_analyze(char):
+    return char == '1' or char == '2' or char == '3' or \
+    char == '4' or char == '5' or char == '6' or \
+    char == '7' or char == '8' or char == '9'
